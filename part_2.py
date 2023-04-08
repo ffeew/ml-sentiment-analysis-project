38e38674de768b6599a9eda0a71cf3294cbbb016
Daniel Koh <aikhong21@gmail.com>
2023-04-08 20:40:50 +0800

    updated viterbi function

--- part_2.py
+++ part_2.py
@@ -73,17 +73,16 @@ def viterbi(sentence: str, transition: dict, emission: gen_e) -> tuple:
     # split the sentence into words
     words = sentence.split("\n")
 
-    # initialize the viterbi matrix
-    viterbi = np.zeros((len(words), len(transition.keys())-1))
-    backpointer = np.zeros((len(words), len(transition.keys())-1))
-
-    # get the states from the transition matrix
     states = [key for key in transition.keys() if key != "START"]
+
+    # initialize the viterbi matrix
+    viterbi = np.zeros((len(words), len(states)))
+    backpointer = np.zeros((len(words), len(states)))
 
     # fill up the start probabilities
     for i, key in enumerate(states):
         viterbi[0, i] = np.log(transition["START"].get(
-            key, 0)) * np.log(emission.get_e(key, words[0]))
+            key, 0) * emission.get_e(key, words[0]))
         backpointer[0, i] = 0
     # fill in the rest of the matrix
     for i in range(1, len(words)):
